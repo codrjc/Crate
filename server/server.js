f68a473ec00c11613950ca415761cc342de1e3a5
Jamie Codrington <codrjc@gmail.com>
2023-07-31 00:09:44 +0100

    connected frontend and backend

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/api", (req, res) => {
+  console.log(req);
+  res.json({
+    users: ["userOne", "userTwo", "userThree", "userFour"],
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
